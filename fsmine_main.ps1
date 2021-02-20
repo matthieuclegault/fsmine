6a1c294bd229de3da378cf2416b80ad0dbf24291
matthieuclegault <36636632+matthieuclegault@users.noreply.github.com>
2021-02-19 21:43:53 -0500

    Update fsmine_main.ps1

--- fsmine_main.ps1
+++ fsmine_main.ps1
@@ -1,9 +1,17 @@
 
+## --Parameters-and-Variables---
+param ([switch] $cool=$true, $fs=$false)
+
+
 
 ## --Functions---
 
 function GetTimeStamp {
     return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+
+function fCooling {
+    #bkebke
 }
 
 function Looping {
@@ -17,12 +25,21 @@ function Looping {
  
  if ($flightsim) {
   Write-Output " LOG:  FlightSimulator is opened, do not start."
+  if ($nsfm) {
+      # try gracefully first
+      $nsfm | Stop-Process
+      # kill after five seconds
+      Sleep 5
+      if (!$nsfm.HasExited) {
+        $nsfm | Stop-Process -Force
+       }
+   }
   Start-Sleep -Seconds $sleeptime
  } elseif ($nsfm) {
   Write-Output " LOG:  Mining soft is opened, sleeping."
   Start-Sleep -Seconds $sleeptime
  } else {
-  Write-Output " starting the mining at $(GetTimeStamp). "
+  Write-Output " `n starting the mining at $(GetTimeStamp). `n "
   cmd /c 'start C:\install\nsfminer.exe -P stratum1+ssl://0x5d6122C69F627f3a6eF4C9C697bE538A065987f4.Slsh@us1.ethermine.org:5555 -U'
   Start-Sleep -Seconds $sleeptime
  }
@@ -45,3 +62,7 @@ while ($true) {
  #  https://geekeefy.wordpress.com/2015/06/11/powershell-infinite-loops-for-testing-purpose/
  #  https://www.gngrninja.com/script-ninja/2016/2/12/powershell-quick-tip-simple-logging-with-timestamps
  #  https://docs.microsoft.com/en-us/powershell/scripting/learn/ps101/09-functions?view=powershell-7.1
+ #  https://www.red-gate.com/simple-talk/sysadmin/powershell/how-to-use-parameters-in-powershell/
+ #  https://stackoverflow.com/questions/14405587/powershell-mandatory-bool-always-true
+ #  https://social.technet.microsoft.com/wiki/contents/articles/23826.powershell-how-to-use-switch-parameter.aspx
+ 
